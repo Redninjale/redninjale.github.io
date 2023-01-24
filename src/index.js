39b6472223ed9467a310b88a021466aa568a6159
Redninjale <redninjale@gmail.com>
2023-01-24 11:32:45 -0600

    Testing Router

--- src/index.js
+++ src/index.js
@@ -8,19 +8,27 @@ import {
   RouterProvider,
   createRoutesFromElements,
   Route,
+  useRouteError
 } from 'react-router-dom';
-// const router = createBrowserRouter([
-//   createRoutesFromElements(
-//     <Route path="/" element={<App/>} />
-//   )
-// ])
+const router = createBrowserRouter([
+  createRoutesFromElements(
+    <Route path="https://redninjale.github.io/" element={<App/>} errorElement={<ErrorBoundary/>}/>
+  )
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      {/* <RouterProvider router={router} /> */}
-    <App/>
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+function ErrorBoundary() {
+  let error = useRouteError();
+  console.error(error);
+  // Uncaught ReferenceError: path is not defined
+  return <div>Dang!</div>;
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
