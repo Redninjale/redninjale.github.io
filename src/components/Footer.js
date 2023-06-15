21e429e9bc6d4f9e3edcb3d84a205ba643046cae
Redninjale <redninjale@gmail.com>
2023-06-14 23:20:52 -0500

    Fixing Manifest

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div className='FooterContainer' id="Footer">
         <div className='FooterGrid'>
             <h1>Contact Me!</h1>
-            <form>
+            {/* <form>
               <div action="https://formsubmit.co/le.ph.cameron@gmail.com" method="POST">
                 <label for="firstName" className="form-label">First Name</label>
                 <input type="text" className="form-control" name="First" id="firstName" required/>
@@ -30,7 +30,7 @@ function Footer() {
               <div>
                 <button type="submit" className="submit">Submit</button>
               </div>
-            </form>
+            </form> */}
             <p>(281)-301-8588 | le.ph.cameron@gmail.com</p>
             <p>Github: <a href="https://github.com/Redninjale" target="_blank">Redninjale</a> | Linkedin: <a href="https://www.linkedin.com/in/cameron-le-09430a24a/" target="_blank">Cameron Le</a></p>
         </div>
